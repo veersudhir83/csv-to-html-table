5fb9ebdf692704fed9aeb5b8a1344962b4c963c2
veersudhir83 <veersudhir83@gmail.com>
2016-06-30 12:38:39 +0530

    Updated script to add logs

--- PublishMetricsToGitHub.sh
+++ PublishMetricsToGitHub.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+echo "Changing to csv-to-html-table project and downloading latest metrics file from artifactory"
 cd d:
 cd gegdc/SV40298/GIT-Repositories/csv-to-html-table/data/
 curl --user 502437115:AP218X3SywtAdgFJLmbnKwwa4LS -O https://devcloud.swcoe.ge.com/artifactory/SXZZG/Applications/PLM-BUILD-METRICS/PLM_BUILD_METRICS.CSV
+echo "Downloaded Metrics file..."
 git add .
 git commit -m "New Metrics File at $(date +"%m-%d-%Y") $(date +"%T")" .
+echo "Uploading metrics file to github and gh-pages"
 git push origin master
 git push origin master:gh-pages
+echo "Done...."
