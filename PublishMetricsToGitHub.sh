7cbdcade48fd04a4c37d1d3b4f59f5bdd4afb0e6
502437115 <sudheer.veeravalli@ge.com>
2016-07-27 14:15:49 +0530

    New Metrics File at 07-27-2016 14:15:48

--- PublishMetricsToGitHub.sh
+++ PublishMetricsToGitHub.sh
@@ -3,6 +3,7 @@ echo "Changing data directory and downloading latest metrics file from artifacto
 cd ./data/
 curl --user 502437115:AP218X3SywtAdgFJLmbnKwwa4LS -O https://devcloud.swcoe.ge.com/artifactory/SXZZG/Applications/PLM-BUILD-METRICS/PLM_BUILD_METRICS.CSV
 echo "Downloaded Metrics file..."
+cd ../
 git add .
 git commit -m "New Metrics File at $(date +"%m-%d-%Y") $(date +"%T")" .
 echo "Uploading metrics file to github and gh-pages"
